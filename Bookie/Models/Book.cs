a3ad7c5257d8356d6f8b9823954779ffca115270
solunska <a_solunska@outlook.com>
2023-06-18 21:08:58 +0200

    first commit

---
+++ Bookie/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookie.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public string Author { get; set; }
+        public string ImageURL { get; set; }
+        public int Price { get; set; }
+        public string Genre { get; set; }
+        public string Language { get; set; }
+
+    }
+}
